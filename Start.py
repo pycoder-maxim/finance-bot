a577c8e8abb983c268116b9865e9a40c2d93b70a
maksimmarkov <markovm2023@yandex.ru>
2025-03-30 18:12:55 +0300

    Доработал команду /help.Сделал 4 кнопки с командами. Также частично прописанна функция ответа  на первую  кнопку - "Добавить доход ". Но она (функция) к сожалению пока не работает, возможно исправлю в след. коммите.

--- Start.py
+++ Start.py
@@ -1,7 +1,13 @@
+from math import lgamma
 from turtledemo.penrose import start
+from xml.sax import parse
 
 import telebot
+
 from telebot import types
+
+
+
 
 bot = telebot.TeleBot('7607516429:AAEpIr2Y0Xwd88iOCOLpVh1I6kC0cRgemLM')
 
@@ -13,30 +19,43 @@ def main(messege):
     bot.send_message(messege.chat.id,'Привет, …! Я помогу вести учёт доходов и расходов. Чтобы узнать доступные команды, введите /help' )
 
 
-
 @bot.message_handler(commands=['help'])
 def info(messege):
-    bot.send_message(messege.chat.id,
-                     'Что-бы добавить доход введите команду /add_income И внесите: <сумма> <категория> <комментарий>**')
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=4)
+    command1 = types.InlineKeyboardButton('1. Добавить доход ♻️',callback_data='add_income')
+    command2 = types.InlineKeyboardButton('2. Добавить расходы 🪫',callback_data='add_res')
+    command3 = types.InlineKeyboardButton('3. Текущий баланс 💰',callback_data='add_m')
+    command4 = types.InlineKeyboardButton('4. Категории 🖼️',callback_data='add_category')
+    markup.add(command1,command2,command3,command4)
+
+    bot.send_message(messege.chat.id,'Данный бот поможет вам оптимизировать ваши расходы с учетом ваших доходов за месяц.'
+                                     '\n'
+                                     '\nСписок команд:'
+                                     '\n'
+                                     '\n<b>1.Добавить доходы</b> - добавляет операцию «Доход»'
+                                     '\n'
+                                     '\n<b>2.Добавить расходы</b> - добавляет операцию «Расход» '
+                                     '\n'
+                                     '\n<b>3.Текущий баланс</b> - показывает текущий баланс (сумма всех доходов минус сумма всех расходов)'
+                                     '\n'
+                                     '\n<b>4.Категори</b> - выводит список доступных категорий для данного пользователя (с разделением на типы, если это предусмотрено)' .format(messege),reply_markup= markup, parse_mode='html')
 
 
-@bot.message_handler(commands=['add_income'])
-def info(messege):
-    bot.send_message(messege.chat.id,
-                     f'Введите ваш доход за месяц : {icome}')
-
-@bot.message_handler()
-def ic(messege):
-    if messege.text.lower() == {icome}:
-        bot.send_message(messege.chat.id,
-                         f'Ваш доход за месяц: {icome}')
+@bot.callback_query_handler(func=lambda call:True)
+def call_back(call):
+    if call.message:
+        if call.data == 'add_income':
+            bot.send_message(call.messege.chat.id, 'Привет')
 
 
 
 
 
 
-bot.polling(none_stop=True, interval=2 )
+
+
+bot.polling(none_stop=True, interval=0 )
 
 
 
