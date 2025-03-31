7ed02640fd0f26569c4dc7a56e71f4d445e3dd65
maksimmarkov <markovm2023@yandex.ru>
2025-04-01 01:57:13 +0300

    Создал глобальные переменные которые хранят 'Доход','Расход', и.т.д Добавил функции действий каждой кнопке кроме - " 4 Категории "

--- Start.py
+++ Start.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot.apihelper import send_message
 
 import info_data
 from telebot import types
@@ -7,8 +8,17 @@ from telebot import types
 
 bot = telebot.TeleBot('7607516429:AAEpIr2Y0Xwd88iOCOLpVh1I6kC0cRgemLM')
 
+#Переменные доходов
+icome = 0
+category = ''
+comment = ''
+#Переменные расходов
+expenditure = 0
+category_r = ''
+comment_r = ''
 
-icome = ''
+
+
 
 @bot.message_handler(commands=['start'])
 def main(messege):
@@ -18,11 +28,12 @@ def main(messege):
 @bot.message_handler(commands=['help'])
 def info(messege):
 
+# Меню бота (кнопки).
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=4)
-    command1 = types.InlineKeyboardButton('1. Добавить доход ♻️',callback_data='add_income')
-    command2 = types.InlineKeyboardButton('2. Добавить расходы 🪫',callback_data='add_res')
-    command3 = types.InlineKeyboardButton('3. Текущий баланс 💰',callback_data='add_m')
-    command4 = types.InlineKeyboardButton('4. Категории 🖼️',callback_data='add_category')
+    command1 = types.InlineKeyboardButton('1. Добавить доход ♻️')
+    command2 = types.InlineKeyboardButton('2. Добавить расходы 🪫')
+    command3 = types.InlineKeyboardButton('3. Текущий баланс 💰')
+    command4 = types.InlineKeyboardButton('4. Категории 🖼️')
     markup.add(command1,command2,command3,command4)
 
     bot.send_message(messege.chat.id,'Данный бот поможет вам оптимизировать ваши расходы с учетом ваших доходов за месяц.'
@@ -35,19 +46,79 @@ def info(messege):
                                      '\n'
                                      '\n<b>3.Текущий баланс</b> - показывает текущий баланс (сумма всех доходов минус сумма всех расходов)'
                                      '\n'
+                
                                      '\n<b>4.Категори</b> - выводит список доступных категорий для данного пользователя (с разделением на типы, если это предусмотрено)' .format(messege),reply_markup= markup, parse_mode='html')
+# Функционал кнопок.
+@bot.message_handler(content_types=['text'])
+def bot_message(messege):
+    if messege.chat.type == 'private':
+        if messege.text == '1. Добавить доход ♻️':
+            bot.send_message(messege.chat.id, 'Введите сумму дохода:' )
+            bot.register_next_step_handler(messege,summa)
+        elif messege.text == '2. Добавить расходы 🪫':
+            bot.send_message(messege.chat.id, 'Введите сумму расхода:')
+            bot.register_next_step_handler(messege, sum_r)
+        elif messege.text == '3. Текущий баланс 💰':
+            bot.register_next_step_handler(messege,ball)
+
+"""Блок обработки по балансу (КНОПКА 3). """
 
 
-@bot.callback_query_handler(func=lambda call:True)
-def call_back(call):
-    if call.message:
-        if call.data == 'add_income':
-            bot.send_message(call.messege.chat.id, 'Привет')
+# Функция подсчета баланса.
+def ball(messege):
+    global icome
+    global expenditure
+    ballance = int(icome) - int(expenditure)
+    bot.send_message(messege.chat.id, f'Ваш текущий балланс: <b>{ballance}</b> руб',
+                     parse_mode='html')
+
+""" Блок обработки по доходам (КНОПКА 1). """
 
 
+# Функция ввести доход.
+def summa(messege):
+    global icome
+    icome = messege.text.strip()
+    bot.send_message(messege.chat.id, 'Введите категорию дохода:')
+    bot.register_next_step_handler(messege, cat)
+
+# Функция ввести категорию дохода.
+def cat(messege):
+    global category
+    category = messege.text.strip()
+    bot.send_message(messege.chat.id, 'Введите комментарий:')
+    bot.register_next_step_handler(messege, com)
+
+# Функция ввести комментарий дохода.
+def com(messege):
+    global comment
+    comment = messege.text.strip()
+    bot.send_message(messege.chat.id, f'Доход <b>{icome} руб</b> по категории <b>{category}</b> добавлен', parse_mode='html')
 
 
+""" Блок обработки по расходам (КНОПКА 2). """
 
+
+# Функция ввести расход.
+def sum_r(messege):
+    global expenditure
+    expenditure = messege.text.strip()
+    bot.send_message(messege.chat.id, 'Введите категорию расхода:')
+    bot.register_next_step_handler(messege, cat_r)
+
+# Функция ввести категорию расхода.
+def cat_r(messege):
+    global category_r
+    category_r = messege.text.strip()
+    bot.send_message(messege.chat.id, 'Введите комментарий:')
+    bot.register_next_step_handler(messege, com_r)
+
+# Функция ввести комментарий расхода.
+def com_r(messege):
+    global comment_r
+    comment_r = messege.text.strip()
+    bot.send_message(messege.chat.id, f'Расход <b>{expenditure} руб</b> по категории <b>{category_r}</b> добавлен',
+                     parse_mode='html')
 
 
 
