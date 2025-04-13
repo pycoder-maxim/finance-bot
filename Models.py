dfc495916ea5792d0278ac291e22546d5b4b9ac2
maksimmarkov <markovm2023@yandex.ru>
2025-04-14 01:30:06 +0300

    Создаем пользователей.

--- Models.py
+++ Models.py
@@ -12,12 +12,31 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    posts = relationship("Post", back_populates="user")
 
     def __repr__(self):
         return f"<User(id={self.id}, name='{self.name}')>"
+
+
+
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    user = relationship("User", back_populates="posts")
+
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}')>"
+
+
+
 
 if __name__ == '__main__':
     engine = create_engine("sqlite:///users.db", echo=True)
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     print("DATABASE CREATED SUCCESSFULLY\n")
+
--- lectures_and_examples.py
+++ lectures_and_examples.py
@@ -10,10 +10,21 @@ Session = sessionmaker(bind= engine)
 session = Session()
 
 user1 = User(name="Alice")
+
 user2 = User(name="Bob")
 session.add_all([user1, user2])
 session.commit()
+post1 = Post(user_id=user.id, title="My first post")
 
 users = session.query(User).all()
 for u in users:
     print(u)
+
+
+post = session.query(Post).first()
+print(post.user.name)
+
+user = session.query(User).first()
+for post in user.posts:
+    print(post.title)
+
