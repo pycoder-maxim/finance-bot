b0d96636938128cb83bbdca10fdc2f85da58a8c0
maksimmarkov <markovm2023@yandex.ru>
2025-04-14 00:38:51 +0300

    Подключаем ORM.

---
+++ Models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DECIMAL, BigInteger
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}')>"
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:////users.db", echo=True)
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    print("DATABASE CREATED SUCCESSFULLY\n")
--- lectures_and_examples.py
+++ lectures_and_examples.py
@@ -9,6 +9,8 @@ project/
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine("sqlite:////users.db", echo=True)
 Base = declarative_base()
@@ -17,3 +19,12 @@ session = Session()
 
 
 
+
+user1 = User(name="Alice")
+user2 = User(name="Bob")
+session.add_all([user1, user2])
+session.commit()
+
+
+for u in users:
+    print(u)
