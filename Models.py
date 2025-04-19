3519e3097ed8ebcb004377fd6a6a52286c7eb251
maksimmarkov <markovm2023@yandex.ru>
2025-04-19 19:08:10 +0300

    Создал модели в SQLalchemy.

--- Models.py
+++ Models.py
@@ -7,32 +7,120 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
-class Expense(Base):
-    __tablename__ = 'expenses'
-
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    comments = relationship("Comment", back_populates="expense")
+    telegramm_id = Column(Integer)
+    first_name = Column(String)
+    last_name = Column(String)
+    user_name = Column(String)
+    created_at = Column(String)
+    categories = relationship("Categories", back_populates="users")
+
+    def __init__(self,telegramm_id:int,first_name:str,last_name:str,user_name:str,created_at:str):
+        self.telegramm_id = telegramm_id
+        self.first_name = first_name[0]
+        self.last_name = last_name[1]
+        self.user_name = user_name[2]
+        self.created_at = created_at
 
     def __repr__(self):
-        return f"<User(id={self.id}, name='{self.name}')>"
+        info: str = f'Telegramm ID: {self.telegramm_id} {self.first_name} {self.last_name} {self.user_name} ,'\
+        f'CREATED AT:{self.created_at}'
+        return info
 
-class Comment(Base):
-    __tablename__ = 'comments'
+
+class Categories(Base):
+    __tablename__ = 'categories'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('expenses.id'), nullable=False)
-    title = Column(String, nullable=False)
-    expenses= relationship("Expense", back_populates="comments")
+    name = Column(String)
+    ctype = Column(String)
+    created_at = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship("Users",back_populates='categories')
+
+    def __init__(self,name:str,ctype:str,created_at:str):
+        self.name = name
+        self.ctype = ctype
+        self.created_at = created_at
 
     def __repr__(self):
-        return f"<User(id={self.id}, post='{self.title}')>"
+        info:str = f'ИМЯ, КАТЕГОРИЯ:{self.name} {self.ctype} {self.created_at}'
+        return info
+
+
+class Walets(Base):
+    __tablename__ = 'walets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    currency = Column(String)
+    created_at = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    walets = relationship("Users", back_populates='walets')
+    def __init__(self,name:str,currency:str,created_at:str):
+        self.name = name
+        self.currency = currency
+        self.created_at = created_at
+
+
+    def __repr__(self):
+        info:str = f'ИМЯ, ВАЛЮТА:{self.name} {self.currency} {self.created_at}'
+        return info
+
+
+class Repoprts(Base):
+    __tablename__ = 'repoprts'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    report_data = Column(String)
+    created_at = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    walets = relationship("Users", back_populates='repoprts')
+    def __init__(self,name:str,report_data:str,created_at:str):
+        self.name = name
+        self.report_data = report_data
+        self.created_at = created_at
+
+
+    def __repr__(self):
+        info:str = f'ИМЯ, ПОВТОРНО ОБРАБОТАННЫЕ ДАННЫЕ:{self.name} {self.currency} {self.created_at}'
+        return info
+
+
+
+class Transactions(Base):
+    __tablename__ = 'transactions'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    report_data = Column(String)
+    created_at = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    walets = relationship("Users", back_populates='transactions')
+    def __init__(self,name:str,report_data:str,created_at:str):
+        self.name = name
+        self.report_data = report_data
+        self.created_at = created_at
+
+
+    def __repr__(self):
+        info:str = f'ИМЯ, ПОВТОРНО ОБРАБОТАННЫЕ ДАННЫЕ:{self.name} {self.currency} {self.created_at}'
+        return info
 
 
 
 
-if __name__ == '__main__':
-    engine = create_engine("sqlite:///users.db", echo=True)
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-    print("DATABASE CREATED SUCCESSFULLY\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
