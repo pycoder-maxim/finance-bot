a031b53686d2bd068396ac9191ac1672a21dbffd
YK <y4roslavkozlov@yande.ru>
2025-06-04 16:55:47 +0300

    Переход на состояния - стадия 6 - удаление лишнего

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
 from loader import bot
-import handlers.start
-import handlers.income
-import handlers.expense
-import handlers.balance
-import handlers.categories
-import handlers.go_back
-from builder.build_all import run_build_script
-
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,3 @@
 from . import start
-from . import income
-from . import expense
 from . import balance
-from . import categories
-from . import go_back
+from . import transactions_states
