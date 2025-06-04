e27688b4a135edc62c060c43314a608c2e7f04db
YK <y4roslavkozlov@yande.ru>
2025-06-04 21:50:59 +0300

    исправил

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 from loader import bot
+import handlers.start
+import handlers.balance
+from builder.build_all import run_build_script
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
